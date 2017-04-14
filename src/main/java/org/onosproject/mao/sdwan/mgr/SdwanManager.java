0259c57ec21e96868e5f6af0a6d72c384dd1341a
Jianwei Mao <maojianwei2020@gmail.com>
2017-04-14 17:49:29 +0800

    design Random value, Incremental and Recursive mechanism to avoid label conflict, Finish

--- src/main/java/org/onosproject/mao/sdwan/mgr/SdwanManager.java
+++ src/main/java/org/onosproject/mao/sdwan/mgr/SdwanManager.java
@@ -16,15 +16,27 @@
 package org.onosproject.mao.sdwan.mgr;
 
 import org.apache.commons.collections.map.MultiValueMap;
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.*;
 import org.onosproject.mao.sdwan.api.SdwanService;
+import org.onosproject.mao.sdwan.api.SdwanTunnel;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.device.DeviceService;
+import org.onosproject.net.topology.TopologyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import static org.onosproject.mao.sdwan.api.SdwanTunnel.INVALID_LABEL;
+import static org.onosproject.mao.sdwan.api.SdwanTunnel.MAX_LABEL;
+import static org.onosproject.net.DeviceId.deviceId;
+import static org.onosproject.net.PortNumber.portNumber;
 
 /**
  * Skeletal ONOS application component.
@@ -33,7 +45,18 @@ import java.util.Map;
 public class SdwanManager implements SdwanService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected DeviceService deviceService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected TopologyService topologyService;
+
     private Map vpnDB = new MultiValueMap();
+    private Set<Integer> usingLabels = new HashSet<>();
+    private Random labelGenerator = new Random();
+
 
     @Activate
     protected void activate() {
@@ -45,6 +68,7 @@ public class SdwanManager implements SdwanService {
     @Deactivate
     protected void deactivate() {
         vpnDB.clear();
+        usingLabels.clear();
 
         log.info("Stopped");
     }
@@ -53,8 +77,93 @@ public class SdwanManager implements SdwanService {
     @Override
     public boolean createVPN(String tenantName, String srcSite, String dstSite) {
 
+        SdwanTunnel.Builder tunnelBuilder = SdwanTunnel.builder().tenant(tenantName);
+
+        ConnectPoint src = generateConnectPoint(srcSite);
+        ConnectPoint dst = generateConnectPoint(dstSite);
+        if(src == null || dst == null) {
+            log.warn("Source or Destination site doesn't match the scheme, e.g. of:0000000000000001/1");
+            return false;
+        }
+
+        tunnelBuilder.src(src).dst(dst);
+
+        Set paths = topologyService.getPaths(topologyService.currentTopology(),
+                src.deviceId(), dst.deviceId());
+        if(paths.isEmpty()) {
+            log.warn("Can't find a route from {} to {}", src.deviceId(), dst.deviceId());
+            return false;
+        }
+
+        // In present, use same route for both unidirectional tunnels.
+
+
 
 
         return false;
+    }
+
+    /**
+     * Return new ConnectPoint object.
+     *
+     * @param dpidPort combine of dpid and port no., e.g. of:0000000000000001/1
+     * @return new ConnectPoint object
+     */
+    private ConnectPoint generateConnectPoint(String dpidPort) {
+
+        String [] dpidAndPort = dpidPort.split("/");
+
+        if(dpidAndPort.length != 2){
+            return null;
+        }
+
+        return new ConnectPoint(deviceId(dpidAndPort[0]), portNumber(dpidAndPort[1]));
+    }
+
+    /**
+     * Get new available label number.
+     *
+     * Adopt random value and incremental mechanism to avoid conflict.
+     *
+     * Default, try to avoid conflict 3 times.
+     *
+     * @return new label number.
+     */
+    private int newLabel() {
+        return newLabel(3);
+    }
+
+    /**
+     * Get new available label number.
+     *
+     * Adopt random value and incremental mechanism to avoid conflict.
+     *
+     * Used in a recursive way.
+     *
+     * @param tryCount times that we find a new random value and try to avoid conflict again
+     * @return new label number.
+     */
+    private int newLabel(int tryCount) {
+
+        if(tryCount <= 0) {
+            return INVALID_LABEL;
+        }
+
+        int label = labelGenerator.nextInt(MAX_LABEL + 1);
+        int freeLabel = label;
+
+        // try to make sure label is not used and available, 10 times.
+        while(usingLabels.contains(freeLabel)
+                && freeLabel - label < 10
+                && freeLabel + 1 <= MAX_LABEL) {
+            freeLabel++;
+        }
+
+        // 10 tries are all failed, find a new random value and try again.
+        if(usingLabels.contains(freeLabel)){
+            return newLabel(tryCount - 1);
+        }
+
+        return freeLabel;
     }
 }
